685d5a633de22ec8f3ed60d0cf9e4e739f43208f
zhaoheting <492995407@qq.com>
2020-02-14 16:00:42 +0800

    Initial a spring boot project with maven.

---
+++ src/main/java/com/example/GrandWorldM/GrandWorldMApplication.java
@@ -0,0 +1,13 @@
+package com.example.GrandWorldM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandWorldMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandWorldMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GrandWorldM/GrandWorldMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GrandWorldM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandWorldMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
