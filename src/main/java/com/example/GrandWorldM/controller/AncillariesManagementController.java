7980bb8434886033bab232fcdbece66a9cd13ffe
zhaoheting <492995407@qq.com>
2020-02-15 21:57:08 +0800

    A initial spring boot project depending on spec, a project generated code with swagger codeGen automatically.

---
+++ src/main/java/com/example/GrandWorldM/controller/AncillariesManagementController.java
@@ -0,0 +1,17 @@
+package com.example.GrandWorldM.controller;
+
+import com.example.GrandWorldMSpec.generated.controller.interfaces.AncillariesManagementApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class AncillariesManagementController implements AncillariesManagementApi {
+
+    @Override
+    public ResponseEntity<String> getAncillaries(@Valid String ancillarieName) {
+        return null;
+    }
+
+}
