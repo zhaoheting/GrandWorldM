e255fd44bb1a5583eec07c48c3af0b56875fe9fa
zhaoheting <492995407@qq.com>
2020-05-09 13:57:22 +0800

    Code base.

--- src/main/java/com/example/GrandWorldM/GrandWorldMApplication.java
+++ src/main/java/com/example/GrandWorldM/GrandWorldMApplication.java
@@ -2,8 +2,12 @@ package com.example.GrandWorldM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.example.GrandWorldM.controllers" })
 public class GrandWorldMApplication {
 
 	public static void main(String[] args) {
