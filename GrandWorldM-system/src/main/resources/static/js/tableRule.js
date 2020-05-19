5555caf31eec6f4a2f10539cfdb5c77ac54b2634
heting.zhao <heting.zhao@openjawtech.com>
2020-05-19 17:54:41 +0800

    Update version bootstrap.

--- GrandWorldM-system/src/main/resources/static/js/tableRule.js
+++ GrandWorldM-system/src/main/resources/static/js/tableRule.js
@@ -1,27 +1,30 @@
-vue = new Vue({
-    el:'#ruleDiv',
-    data:{
-        tableRuleList: []
-    },
-//    mounted(){
-//        axios
-//            .get('http://localhost:8080/rule/table')
-//            .then(response=> (this.tableRuleList= response.tableRuleList))
-//            .catch(function (error) {
-//                    console.log(error);
-//            });
-//    },
-    methods:{
-        getTableRuleList:function(){
-            //发送get请求
-            this.$http.get('http://localhost:8080/rule/table').then(function(result){
-                  tableRuleList= result;
-            },function(){
-                console.log('请求失败处理');
-            });
+window.onload = function(){
+    var vue = new Vue({
+        el:'#ruleDiv',
+        data:{
+            tableRuleList: []
+        },
+    //    mounted(){
+    //        axios
+    //            .get('http://localhost:8080/rule/table')
+    //            .then(response=> (this.tableRuleList= response.tableRuleList))
+    //            .catch(function (error) {
+    //                    console.log(error);
+    //            });
+    //    },
+        methods:{
+            getTableRuleList:function(){
+                //发送get请求
+                this.$http.get('http://localhost:8080/rule/table').then(function(result){
+                      tableRuleList= result;
+                },function(){
+                    console.log('请求失败处理');
+                });
+            }
         }
-    }
-})
+    });
+}
+
 
 //function getTableRuleList(){
 //    $.ajax({
