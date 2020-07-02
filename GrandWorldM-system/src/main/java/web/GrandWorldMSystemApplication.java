656f5e140a2d6e3384a86fda983052243e924890
heting.zhao <heting.zhao@openjawtech.com>
2020-07-02 15:40:48 +0800

    Update name of application.

---
+++ GrandWorldM-system/src/main/java/web/GrandWorldMSystemApplication.java
@@ -0,0 +1,15 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication()
+public class GrandWorldMSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandWorldMSystemApplication.class, args);
+	}
+
+}
