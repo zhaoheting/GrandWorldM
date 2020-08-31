330e7497121b7bcd273477272a8218d0635dfd9d
zhaoheting <492995407@qq.com>
2020-08-31 23:12:33 +0800

    Remove

--- GrandWorldM-system/src/main/java/web/services/learn/chapter/four/BinarySearchTree.java
+++ GrandWorldM-system/src/main/java/web/services/learn/chapter/four/BinarySearchTree.java
@@ -14,6 +14,17 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
      * The root node of the tree.
      */
     private BinaryNode<AnyType> root;
+    /**
+     * 当前没被标记为已删除的节点数。
+     */
+    private int theSize;
+    /**
+     * 已经被懒惰删除的节点数。（标记为删除，但没有真的删除）
+     */
+    private int deletedSize;
+    private BinaryNode<AnyType> min;
+    private BinaryNode<AnyType> max;
+
 
     public boolean contains(AnyType x) {
         return contains(x, root);
@@ -86,7 +97,7 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
     private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> node) {
         if (node == null) {
             //节点为空的情况是递归的边界情况，每个insert操作都会终止于当前node为空时。
-            BinaryNode<AnyType> root = new BinaryNode<>(x);
+            node = new BinaryNode<>(x);
         } else {
             int compareResult = x.compareTo(node.element);
             if (compareResult < 0) {
@@ -101,7 +112,51 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
                 node.occurrence++;
             }
         }
-        return root;
+        return node;
+    }
+
+    public BinaryNode<AnyType> remove(AnyType x) {
+
+    }
+
+    /**
+     * A inefficient way to remove,comparing with lazy deletion.
+     *
+     * @return
+     */
+    private BinaryNode<AnyType> removeNode(AnyType x, BinaryNode<AnyType> node) {
+        if (node == null) {
+            throw new RuntimeException("The tree is null, can't find x.");
+        }
+        int comparedResult = x.compareTo(node.element);
+        if (comparedResult < 0) {
+            removeNode(x, node.left);
+        } else if (comparedResult > 0) {
+            removeNode(x, node.right);
+        } else {
+            /**
+             * 当前找到的节点，既有左子树，也有右子树。
+             */
+            if (node.left != null && node.right != null) {
+
+            }else {//包含了当前节点是叶子，以及当前节点只有一个子节点的情况。
+                node = (node.left != null) ? node.left: node.right;
+            }
+        }
+    }
+
+    /**
+     * Lazy deletion.
+     * 懒惰删除（英文：lazy deletion）指的是从一个散列表（也称哈希表）中删除元素的一种方法。
+     * 在这个方法中，删除仅仅是指标记一个元素被删除，而不是整个清除它。
+     * 被删除的位点在插入时被当作空元素，在搜索之时被当作已占据。
+     *
+     * @param x
+     * @param node
+     * @return
+     */
+    private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> node) {
+
     }
 
     /**
@@ -116,17 +171,23 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
         BinaryNode<AnyType> right;
         //优化，记录树中该节点出现次数。
         int occurrence;
+        int deleteFlag;
 
         public BinaryNode(AnyType element) {
-            this(element, null, null, 1);
+            this(element, null, null, 1, 1);
+        }
+
+        public BinaryNode(AnyType element, BinaryNode<AnyType> left, BinaryNode<AnyType> right) {
+            this(element, left, right, 1, 1);
         }
 
         private BinaryNode(AnyType element, BinaryNode<AnyType> left,
-                           BinaryNode<AnyType> right, int occurrence) {
+                           BinaryNode<AnyType> right, int occurrence, int deleteFlag) {
             this.element = element;
             this.left = left;
             this.right = right;
             this.occurrence = occurrence;
+            this.deleteFlag = deleteFlag;
         }
     }
 }
