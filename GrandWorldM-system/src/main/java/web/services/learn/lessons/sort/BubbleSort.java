899c3b35386d538853368047d3d07a22ea147a91
heting.zhao <heting.zhao@openjawtech.com>
2020-09-21 16:05:59 +0800

    Amend bubble sort.

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/BubbleSort.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/BubbleSort.java
@@ -6,8 +6,8 @@ package web.services.learn.lessons.sort;
 public class BubbleSort {
 
     private static void sort(int[] a) {
-        for (int i = 0, length = a.length; i < length; i++) {
-            for (int j = 0; j < length - i - 1; j++) {
+        for (int i = 1, length = a.length; i < length; i++) {
+            for (int j = 0; j < length - i; j++) {
                 if (a[j] > a[j + 1]) {
                     int mid = a[j];
                     a[j] = a[j + 1];
@@ -18,7 +18,7 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] a = new int[]{4,6,2,5,7,3};
+        int[] a = new int[]{6, 4, 7, 3, 2, 34};
         //传入方法的参数只要是地址，那么方法执行完后，数据的更新会一直存在。如果是基本数据类型，那么就不会
         sort(a);
         for (int n : a) {
