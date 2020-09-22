5e6390dbf49f6878bb393a29490a435c179d67d8
zhaoheting <492995407@qq.com>
2020-09-22 22:40:56 +0800

    Merge with sentinel.

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/MergeSort.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/MergeSort.java
@@ -1,7 +1,9 @@
 package web.services.learn.lessons.sort;
 
+import io.swagger.models.auth.In;
+
 /**
- * 归并排序，没写出来，有问题
+ * 归并排序。
  */
 public class MergeSort {
     public static void main(String[] args) {
@@ -24,7 +26,7 @@ public class MergeSort {
         mergeSort(a, start, mid);
         mergeSort(a, mid + 1, end);
         //当start和end之间只有两个元素时，调用combine函数就直接比较出了大小顺序。所以真正的排序发生在combine函数中。
-        combine(a, start, mid, end);
+        combineSentinel(a, start, mid, end);
     }
 
     /**
@@ -57,6 +59,29 @@ public class MergeSort {
         } else {
             for (int d = m; d < mid - start + 1; d++) {
                 a[t++] = left[d];
+            }
+        }
+    }
+
+    private static void combineSentinel(int[] a, int start, int mid, int end){
+        //将start到end之间的元素拆分到两个数组中。
+        int[] left = new int[mid - start + 2];
+        int[] right = new int[end - mid+1];
+        for (int i = 0; i < mid - start + 1; i++) {
+            left[i] = a[start + i];
+        }
+        for (int j = 0; j < end - mid; j++) {
+            right[j] = a[mid + j + 1];
+        }
+        left[left.length-1]= Integer.MAX_VALUE;
+        right[right.length-1] = Integer.MAX_VALUE;
+        //将拆分的两个数组中的元素按照大小放入a中。
+        int m = 0, n = 0, t = start;
+        while (left[m] != Integer.MAX_VALUE) {
+            if (left[m] < right[n]) {
+                a[t++] = left[m++];
+            } else {
+                a[t++] = right[n++];
             }
         }
     }
