74fb4d7bc87eada956d1bede85c635647535f471
heting.zhao <heting.zhao@openjawtech.com>
2020-09-23 11:27:10 +0800

    Merge sort, using sentinel to simplify the judgement.

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/MergeSort.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/MergeSort.java
@@ -1,13 +1,12 @@
 package web.services.learn.lessons.sort;
 
-import io.swagger.models.auth.In;
-
 /**
  * 归并排序。
  */
 public class MergeSort {
     public static void main(String[] args) {
-        int[] a = new int[]{5, 3, 6, 7, 2, 4, 9, 1};
+//        int[] a = new int[]{5, 3, 6, 7, 2, 4, 9, 1};
+        int[] a = new int[]{2, 5, 3, 4};
         sort(a);
         for (int n : a) {
             System.out.println(n);
@@ -63,21 +62,32 @@ public class MergeSort {
         }
     }
 
-    private static void combineSentinel(int[] a, int start, int mid, int end){
+    /**
+     * Merge sort, using sentinel to simplify the judgement.
+     *
+     * @param a
+     * @param start
+     * @param mid
+     * @param end
+     */
+    private static void combineSentinel(int[] a, int start, int mid, int end) {
         //将start到end之间的元素拆分到两个数组中。
         int[] left = new int[mid - start + 2];
-        int[] right = new int[end - mid+1];
+        int[] right = new int[end - mid + 1];
         for (int i = 0; i < mid - start + 1; i++) {
             left[i] = a[start + i];
         }
         for (int j = 0; j < end - mid; j++) {
             right[j] = a[mid + j + 1];
         }
-        left[left.length-1]= Integer.MAX_VALUE;
-        right[right.length-1] = Integer.MAX_VALUE;
+        left[left.length - 1] = Integer.MAX_VALUE;
+        right[right.length - 1] = Integer.MAX_VALUE;
         //将拆分的两个数组中的元素按照大小放入a中。
         int m = 0, n = 0, t = start;
-        while (left[m] != Integer.MAX_VALUE) {
+        //此处循环条件必须是对左侧数组的最大值判断。不能是对右侧数组的最大值判断。在左侧数组先被遍历完的情况，
+        // 因为右侧数组原本就在原数组的右侧位，所以这种情况右侧数组剩余的大的值也不用挪到原数组中，他们原来的位置就是对的位置。
+        //另外不等于判断的性能要优于等于判断，因为不等于判断只需要找到一个不同的二进制位即可，而等于判断要比较完所有的二进制位。
+        while (left[n] != Integer.MAX_VALUE) {
             if (left[m] < right[n]) {
                 a[t++] = left[m++];
             } else {
