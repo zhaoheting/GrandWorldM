d9c7a55291936c109404502e8a43367f66cb9c5e
heting.zhao <heting.zhao@openjawtech.com>
2020-10-12 17:31:38 +0800

    Add implementation of for loop.

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/ShellSort.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/sort/ShellSort.java
@@ -11,6 +11,7 @@ public class ShellSort {
             for (int i = step; i < length; i++) {
                 int temp = a[i];
                 int j = i - step;
+                //while loop.
                 while (j >= 0 && temp < a[j]) {
                     a[j + step] = a[j];
                     j -= step;
@@ -22,9 +23,28 @@ public class ShellSort {
 
     public static void main(String[] args) {
         int[] a = new int[]{5, 3, 6, 4, 2, 1};
-        sort(a);
+        sort2(a);
         for (int n : a) {
             System.out.println(n);
+        }
+    }
+
+    private static void sort2(int[] a) {
+        int length = a.length;
+        for (int step = length >> 1; step >= 1; step /= 2) {
+            for (int i = step; i < length; i++) {
+                int temp = a[i];
+                int j = i - step;
+                //for loop.
+                for (; j >= 0; j -= step) {
+                    if (a[j] > temp) {
+                        a[j + step] = a[j];
+                    } else {
+                        break;
+                    }
+                }
+                a[j + step] = temp;
+            }
         }
     }
 }
