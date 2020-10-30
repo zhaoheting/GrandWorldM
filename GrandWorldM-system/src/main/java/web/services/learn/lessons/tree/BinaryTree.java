fbb5e2ed3c4556edb9d10a3ed12882b6a15555a6
zhaoheting <492995407@qq.com>
2020-10-30 14:01:46 +0800

    第一轮面试

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/tree/BinaryTree.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/tree/BinaryTree.java
@@ -1,10 +1,6 @@
 package web.services.learn.lessons.tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Common module to traverse binary tree.
@@ -15,33 +11,44 @@ public class BinaryTree<E> {
     BinaryTreeNode<E> root;
 
     public static void main(String[] args) {
-        BinaryTreeNode node10 = new BinaryTreeNode(10, null, null);
-        BinaryTreeNode node8 = new BinaryTreeNode(8, null, null);
-        BinaryTreeNode node9 = new BinaryTreeNode(9, null, node10);
-        BinaryTreeNode node4 = new BinaryTreeNode(4, null, null);
-        BinaryTreeNode node5 = new BinaryTreeNode(5, node8, node9);
-        BinaryTreeNode node6 = new BinaryTreeNode(6, null, null);
+//        BinaryTreeNode node10 = new BinaryTreeNode(10, null, null);
+//        BinaryTreeNode node8 = new BinaryTreeNode(8, null, null);
+//        BinaryTreeNode node9 = new BinaryTreeNode(9, null, node10);
+//        BinaryTreeNode node4 = new BinaryTreeNode(4, null, null);
+//        BinaryTreeNode node5 = new BinaryTreeNode(5, node8, node9);
+        BinaryTreeNode node6 = new BinaryTreeNode(15, null, null);
         BinaryTreeNode node7 = new BinaryTreeNode(7, null, null);
-        BinaryTreeNode node2 = new BinaryTreeNode(2, node4, node5);
-        BinaryTreeNode node3 = new BinaryTreeNode(3, node6, node7);
+        BinaryTreeNode node2 = new BinaryTreeNode(9, null, null);
+        BinaryTreeNode node3 = new BinaryTreeNode(20, node6, node7);
         BinaryTree<Integer> binaryTree = new BinaryTree<>();
-        binaryTree.root = new BinaryTreeNode(1, node2, node3);
-        System.out.println("previous order:");
-        binaryTree.preOrder();
-        System.out.println();
-        binaryTree.preOrderNoRecursive();
-        System.out.println("\nin order:");
-        binaryTree.inOrder();
-        System.out.println();
-        binaryTree.inOrderNoRecursive();
-        System.out.println("\npost order:");
-        binaryTree.postOrder();
-        System.out.println("\nlevel order:");
-        binaryTree.levelOrder();
-        System.out.println("\ntraverse the tree in 5 levels:");
-        List<List<Integer>> result = binaryTree.levelOrderInMultiList();
-        for (int k = 0; k < result.size(); k++) {
-            List<Integer> currentLevel = result.get(k);
+        binaryTree.root = new BinaryTreeNode(3, node2, node3);
+//        System.out.println("previous order:");
+//        binaryTree.preOrder();
+//        System.out.println();
+//        binaryTree.preOrderNoRecursive();
+//        System.out.println("\nin order:");
+//        binaryTree.inOrder();
+//        System.out.println();
+//        binaryTree.inOrderNoRecursive();
+//        System.out.println("\npost order:");
+//        binaryTree.postOrder();
+//        System.out.println("\nlevel order:");
+//        binaryTree.postOrderWithIteration();
+//        System.out.println("\nlevel order:");
+//        binaryTree.levelOrder();
+//        System.out.println("\ntraverse the tree in 5 levels:");
+//        List<List<Integer>> result = binaryTree.levelOrderInMultiList();
+//        for (int k = 0; k < result.size(); k++) {
+//            List<Integer> currentLevel = result.get(k);
+//            for (int j = 0; j < currentLevel.size(); j++) {
+//                System.out.print(currentLevel.get(j) + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        List<List<Integer>> result2 = binaryTree.levelOrderBottom();
+        for (int k = 0; k < result2.size(); k++) {
+            List<Integer> currentLevel = result2.get(k);
             for (int j = 0; j < currentLevel.size(); j++) {
                 System.out.print(currentLevel.get(j) + "\t");
             }
@@ -122,6 +129,33 @@ public class BinaryTree<E> {
         }
     }
 
+    private void postOrderWithIteration() {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return;
+        }
+        BinaryTreeNode node = root;
+        Deque<BinaryTreeNode> stack = new LinkedList<BinaryTreeNode>();
+        BinaryTreeNode prev = null;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            if (node.right == null || node.right == prev) {
+                System.out.print(node.data + "\t");
+                prev = node;
+                node = null;
+            } else {
+                stack.push(node);
+                node = node.right;
+            }
+            Queue<Integer> queue = new LinkedList<>();
+            queue.size();
+        }
+}
+
     /**
      * Traverse binary tree in level order and all the numbers of one level are saved in a single array.
      */
@@ -172,5 +206,36 @@ public class BinaryTree<E> {
                 queue.offer(right);
             }
         }
+    }
+
+    public List<List<E>> levelOrderBottom() {
+        List<List<E>> resultList = new ArrayList<>();
+        Queue<BinaryTreeNode<E>> nodeQueue = new LinkedList<>();
+        BinaryTreeNode<E> node = root;
+        if (node != null) {
+            nodeQueue.offer(node);
+        }
+        Stack<List<E>> listStack = new Stack<>();
+        while (!nodeQueue.isEmpty()) {
+            List<E> currentLevel = new ArrayList<>();
+            int queueLength = nodeQueue.size();
+            for (int i = 0; i < queueLength; i++) {
+                node = nodeQueue.poll();
+                currentLevel.add(node.data);
+                BinaryTreeNode<E> left = node.left;
+                BinaryTreeNode<E> right = node.right;
+                if (left != null) {
+                    nodeQueue.offer(left);
+                }
+                if (right != null) {
+                    nodeQueue.offer(right);
+                }
+            }
+            listStack.push(currentLevel);
+        }
+        for (int j = 0; j <= listStack.size(); j++) {
+            resultList.add(listStack.pop());
+        }
+        return resultList;
     }
 }
