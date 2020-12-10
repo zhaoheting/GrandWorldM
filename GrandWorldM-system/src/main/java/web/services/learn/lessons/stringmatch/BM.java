c14b5d5ac13a80608623eecd3bd71355b00f2c7a
heting.zhao <heting.zhao@openjawtech.com>
2020-12-10 17:54:51 +0800

    string match.

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/stringmatch/BM.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/stringmatch/BM.java
@@ -8,24 +8,32 @@ public class BM {
     public int match(char[] originArr, char[] targetArr) {
         int oLength = originArr.length;
         int tLength = targetArr.length;
+        int[] targetAscii = generateBadChar(targetArr);
         int i = 0;//i is used to mark position that the first char of the target array is moved backwards to.
         while (i < oLength - tLength) {
             //compare every char from the end.
-            int j = tLength - 1+i;
+            int j = tLength - 1;
             for (; j >= 0; --j) {
-                if()
+                if (originArr[j + i] != targetArr[j]) {
+                    break;
+                }
             }
             //if all are the same, break.
+            if (j < 0) {
+                return i;
+            }
             //else, get the index current char of origin array and
+            i = i + (j - targetAscii[(int) targetArr[j - i]]);
             // move the target array backward by a number related to the current index.
         }
+        return -1;
     }
 
     /**
      * @param arr
      * @return
      */
-    private int[] genTargetArr(char[] arr) {
+    private int[] generateBadChar(char[] arr) {
         int[] targetArr = new int[SIZE];
         //initialization.
         for (int i = 0; i < arr.length; i++) {
@@ -37,5 +45,27 @@ public class BM {
             targetArr[ascii] = i;
         }
         return targetArr;
+    }
+
+    private void generateGoodSuffix(char[] targetArr) {
+        int targetLength = targetArr.length;
+        int[] suffix = new int[targetLength];
+        boolean[] isPrefix = new boolean[targetLength];
+        for (int i = 1; i < targetLength; ++i) {
+            suffix[i] = -1;
+            isPrefix[i] = false;
+        }
+        for (int i = 0; i < targetLength - 1; ++i) {
+            int j = i;
+            int k = 0;
+            while (j >= 0 && targetArr[j] == targetArr[targetLength - 1 - k]) {
+                --j;
+                ++k;
+                suffix[k] = j + 1;
+            }
+            if(j == -1){
+                isPrefix[k] = true;
+            }
+        }
     }
 }
