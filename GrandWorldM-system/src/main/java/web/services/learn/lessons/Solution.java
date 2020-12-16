6c2b5f7f89219ec2bc6c80b99645d34499ef6a54
zhaoheting <492995407@qq.com>
2020-12-17 00:12:16 +0800

    n queens.

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/Solution.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/Solution.java
@@ -2,34 +2,72 @@ package web.services.learn.lessons;
 
 import io.swagger.models.auth.In;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Solution {
 
     public static void main(String[] args) {
-        String result = strWithout3a3b(4,1);
-        System.out.println(result);
+        solveNQueens(4);
     }
-    public static String strWithout3a3b(int A, int B) {
-        StringBuilder result = new StringBuilder();
-        while(A>0 || B>0){
-            boolean isA = false;
-            int length = result.length();
-            if(length >= 2 && result.charAt(length-2) == result.charAt(length-1)){
-                if(result.charAt(length-1) == 'b'){
-                    isA = true;
-                }
-            } else{
-                if(A>B){
-                    isA = true;
-                }
-            }
-            if(isA){
-                result.append("a");
-                --A;
-            } else {
-                result.append("b");
-                --B;
-            }
+    public static List<List<String>> solveNQueens(int n) {
+        int[] matrix = new int[n];
+        Set<Integer> columnSet = new HashSet<>();
+        Set<Integer> leftDiagonalSet = new HashSet<>();
+        Set<Integer> rightDiagonalSet = new HashSet<>();
+        List<List<String>> resultList = new ArrayList<>();
+        backTrack(0, n, matrix, columnSet, leftDiagonalSet, rightDiagonalSet, resultList);
+        return resultList;
+
+    }
+    private static void backTrack(int row, int n,
+                           int[] matrix,
+                           Set<Integer> columnSet,
+                           Set<Integer> leftDiagonalSet,
+                           Set<Integer> rightDiagonalSet,
+                           List<List<String>> resultList){
+        if(row == n){
+            resultList.add(getCurrentSolution(matrix));
+            columnSet.clear();
+            leftDiagonalSet.clear();
+            rightDiagonalSet.clear();
+            return;
         }
-        return result.toString();
+        for(int column = 0; column < n; ++column){
+            if(columnSet.contains(column)){
+                continue;
+            }
+            int x = row - column;
+            int y = row + column;
+            if(leftDiagonalSet.contains(x)){
+                continue;
+            }
+            if(rightDiagonalSet.contains(y)){
+                continue;
+            }
+            matrix[row] = column;
+            columnSet.add(column);
+            leftDiagonalSet.add(x);
+            rightDiagonalSet.add(y);
+            backTrack(row+1, n, matrix, columnSet, leftDiagonalSet, rightDiagonalSet,resultList);
+        }
+    }
+
+    private static List<String> getCurrentSolution(int[] matrix){
+        List<String> result = new ArrayList<>();
+        for(int row =0; row< matrix.length; ++row){
+            StringBuilder currentLine = new StringBuilder();
+            for(int column= 0; column < matrix.length; ++column){
+                if(matrix[row] == column){
+                    currentLine.append("Q");
+                } else{
+                    currentLine.append(".");
+                }
+            }
+            result.add(currentLine.toString());
+        }
+        return result;
     }
 }
