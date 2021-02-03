a83e11982ce1163e19c8a8ea4960e39949d78936
heting.zhao <heting.zhao@openjawtech.com>
2021-02-03 17:14:57 +0800

    三数和等于零

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/Solution.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/Solution.java
@@ -1,29 +1,57 @@
 package web.services.learn.lessons;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
     public static void main(String[] args) {
-        int[] arr = {1,3,5,7,2,4,6,8};
-        smallestK(arr,4);
-
+//        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int[] nums = {1, -1, -1, 0};
+        List<List<Integer>> result = threeSum(nums);
+        System.out.print(result);
     }
 
-    static Comparator<Integer> cmp = (e1, e2) -> e2 - e1;
-
-    public static int[] smallestK(int[] arr, int k) {
-        Queue<Integer> queue = new PriorityQueue<>(k, cmp);
-        for (int i = 0; i < k; ++i) {
-            queue.offer(arr[i]);
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int length = nums.length;
+        if (length < 3) {
+            return result;
         }
-        int length = arr.length;
-        for (int j = k; j < length; ++j) {
-            if (queue.peek() > arr[k]) {
-                queue.poll();
-                queue.offer(arr[j]);
+        if (nums[0] > 0 || nums[length - 1] < 0) {
+            return result;
+        }
+        for (int i = 0; i < length; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int leftIndex = i + 1;
+            int rightIndex = length - 1;
+            while (leftIndex < rightIndex) {
+                int currentSum = nums[i] + nums[leftIndex] + nums[rightIndex];
+                if (currentSum == 0) {
+                    List<Integer> currentList = new ArrayList<>();
+                    currentList.add(nums[i]);
+                    currentList.add(nums[leftIndex]);
+                    currentList.add(nums[rightIndex]);
+                    result.add(currentList);
+                    while (leftIndex + 1 < rightIndex && nums[leftIndex + 1] == nums[leftIndex]) {
+                        ++leftIndex;
+                    }
+                    while (rightIndex - 1 >= leftIndex && nums[rightIndex - 1] == nums[rightIndex]) {
+                        --rightIndex;
+                    }
+                    ++leftIndex;
+                    --rightIndex;
+                } else if (currentSum < 0) {
+                    ++leftIndex;
+                } else {
+                    --rightIndex;
+                }
             }
         }
-        return queue.stream().mapToInt(Integer::intValue).toArray();
+        return result;
     }
 }
