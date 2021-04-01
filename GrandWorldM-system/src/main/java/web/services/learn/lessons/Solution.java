79e84fd1b03bcdaa483235f13ea2475a61aaa711
heting.zhao <heting.zhao@openjawtech.com>
2021-04-01 15:14:39 +0800

    微软面试题-判断excell表中的单元格是否有循环依赖。

--- GrandWorldM-system/src/main/java/web/services/learn/lessons/Solution.java
+++ GrandWorldM-system/src/main/java/web/services/learn/lessons/Solution.java
@@ -1,38 +1,49 @@
 package web.services.learn.lessons;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 class Solution {
 
-    /**
-     * 找出正整数数组中最大的元素数量等于元素本身的元素，爱立信面试题(自己写的不一定对)。
-     *
-     * @param args
-     */
+    static boolean result = false;
+
     public static void main(String[] args) {
-        int[] arr = {2, 1, 3, 4, 2, 3, 3,3,4,4,4};
-        Arrays.sort(arr);
-        int[] variables = new int[3];//variables[0] is current number. variables[1] is the count of current number.
-        //variable[2] is the result;
-        variables[0] = arr[0];
-        variables[2] = Integer.MIN_VALUE;
-        Arrays.stream(arr).forEach(x -> {
-            if (x == variables[0]) {
-                ++variables[1];
-            } else {
-                if (variables[0] == variables[1]) {
-                    variables[2] = Math.max(variables[2], variables[0]);
-                } else {
-                    variables[2] = Math.max(variables[2], -1);
-                }
-                variables[0] = x;
-                variables[1] = 1;
-            }
-        });
-        if (variables[0] == variables[1] && variables[0] != variables[2]) {
-            variables[2] = variables[0];
-        }
-        System.out.println(variables[2]);
+        String[] param = {"", ""};
     }
 
+    private boolean check(String[] strings) {
+        Map<String, Set<String>> map = new HashMap<>();
+        int length = strings.length;
+        for (int index = 0; index < length; ++index) {
+            String key = strings[index].substring(0, strings[index].indexOf("="));
+            String[] values = strings[index].substring(strings[index].indexOf("=") + 1).split("");
+            Set<String> valueSet = new HashSet<>();
+            Set<String> keySet = new HashSet<>();
+            for (String value : values) {
+                if (map.containsKey(value)) {
+                    recurse(map, result, value, keySet);
+                } else {
+                    valueSet.add(value);
+                }
+            }
+            map.put(key, valueSet);
+        }
+        return result;
+    }
+
+    private boolean recurse(Map<String, Set<String>> map, boolean result, String value, Set<String> keySet) {
+        if (!map.containsKey(value)) {
+            //recurse.
+            return result;
+        }
+        Set<String> currentSet = map.get(value);
+        for (String current : currentSet) {
+            keySet.add(current);
+            recurse(map, result, current, keySet);
+            keySet.remove(current);
+        }
+        return result;
+    }
 }
