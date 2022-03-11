6edc0287ef30dbd1e274a071e1f56270eed7817e
hobbs <hobbs.heting.zhao@kingland.com>
2022-03-11 14:25:25 +0800

    股票买卖问题通用解法。

--- GrandWorldM-system/src/main/java/web/services/learn/LoveFlyWithDream.java
+++ GrandWorldM-system/src/main/java/web/services/learn/LoveFlyWithDream.java
@@ -7,34 +7,31 @@ import java.util.List;
 public class LoveFlyWithDream {
 
     public static void main(String[] args) {
-
+        int[] prices = {3, 2, 6, 5, 0, 3};
+        LoveFlyWithDream loveFlyWithDream = new LoveFlyWithDream();
+        int result = loveFlyWithDream.sss(prices, 2);
+        System.out.println(result);
     }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        backTrack(0, candidates, target, resultList, new ArrayList<>());
-        return resultList;
-    }
-
-    private void backTrack(int index, int[] candidates, int target, List<List<Integer>> resultList, List<Integer> currentList) {
-        int currentSum = currentList.stream().mapToInt(x -> x).sum();
-        if (currentSum == target) {
-            resultList.add(new ArrayList<>(currentList));
-            return;
+    private int sss(int[] prices, int k) {
+        int length = prices.length;
+        int[][] dp = new int[k + 1][2];
+        //base case.
+        // K must be larger than 1, or it is meaningless. So we just define dp[0][0][0] specially for calculating state.
+        dp[0][0] = 0; //This line can be omitted, because the default value is 0.
+        for (int m = k; m >= 1; --m) {
+            dp[m][0] = 0;
+            dp[m][1] = -prices[0];
         }
-        int[] currentCandidates = Arrays.copyOfRange(candidates, index, candidates.length);
-        int min = Arrays.stream(currentCandidates).min().getAsInt();
-        if (target - currentSum < min) {
-            return;
-        }
-        int length = candidates.length;
-        for (int i = index; i < length; ++i) {
-            if (currentSum + candidates[i] <= target) {
-                currentList.add(candidates[i]);
-                backTrack(i, candidates, target, resultList, currentList);
-                currentList.remove(currentList.size() - 1);
+        //dp[i][k][0] = max(dp[i-1][k][0], dp[i-][k][1] + prices[i]);
+        //dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i];
+        for (int i = 1; i < length; ++i) {
+            for (int j = k; j >= 1; --j) {
+                dp[j][0] = Math.max(dp[j][0], dp[j][1] + prices[i]);
+                dp[j][1] = Math.max(dp[j][1], dp[j - 1][0] - prices[i]);
             }
         }
+        return dp[k][0];
     }
 
     private static class Node {
