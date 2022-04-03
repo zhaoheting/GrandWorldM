5a13c15e711eddbb663cd020d78d961859487a05
zhaoheting <492995407@qq.com>
2022-04-03 12:15:00 +0800

    微策略面试题

--- GrandWorldM-system/src/main/java/web/services/learn/LoveFlyWithDream.java
+++ GrandWorldM-system/src/main/java/web/services/learn/LoveFlyWithDream.java
@@ -1,49 +1,81 @@
 package web.services.learn;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LoveFlyWithDream {
 
+    /**
+     * 微策略面试题，判断字符串是否平衡。
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        int[] prices = {3, 2, 6, 5, 0, 3};
+        String s1 = "{()}[]";
+        List<String> result = new ArrayList<>();
+        result.add(s1);
         LoveFlyWithDream loveFlyWithDream = new LoveFlyWithDream();
-        int result = loveFlyWithDream.sss(prices, 2);
-        System.out.println(result);
+        List<String> res = loveFlyWithDream.braces(result);
+        System.out.println(res);
     }
 
-    private int sss(int[] prices, int k) {
-        int length = prices.length;
-        int[][] dp = new int[k + 1][2];
-        //base case.
-        // K must be larger than 1, or it is meaningless. So we just define dp[0][0][0] specially for calculating state.
-        dp[0][0] = 0; //This line can be omitted, because the default value is 0.
-        for (int m = k; m >= 1; --m) {
-            dp[m][0] = 0;
-            dp[m][1] = -prices[0];
-        }
-        //dp[i][k][0] = max(dp[i-1][k][0], dp[i-][k][1] + prices[i]);
-        //dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i];
-        for (int i = 1; i < length; ++i) {
-            for (int j = k; j >= 1; --j) {
-                dp[j][0] = Math.max(dp[j][0], dp[j][1] + prices[i]);
-                dp[j][1] = Math.max(dp[j][1], dp[j - 1][0] - prices[i]);
+
+    private List<String> braces(List<String> values) {
+        int length = values.size();
+        List<String> resultList = new ArrayList<>();
+        for (int i = 0; i < length; ++i) {
+            String current = values.get(i);
+            if (current.length() % 2 != 0) {
+                resultList.add("NO");
+            }
+            if (isBalance(current)) {
+                resultList.add("YES");
+            } else {
+                resultList.add("NO");
             }
         }
-        return dp[k][0];
+        return resultList;
+    }
+
+    private boolean isBalance(String value) {
+        int length = value.length();
+        boolean[][] isBan = new boolean[length][length];
+        ///init
+        for (int i = 0; i < length; ++i) {
+            for (int j = 0; j < length; ++j) {
+                if (i == j) {
+                    isBan[i][j] = false;
+                }
+                if (i > j) {
+                    isBan[i][j] = true;
+                }
+            }
+        }
+
+        for (int c = 1; c < length; ++c) {
+            for (int r = c - 1; r >= 0; --r) {
+                if (((c < 2 || isBan[r][c - 2]) && check(value.charAt(c - 1), value.charAt(c))) || (isBan[r + 1][c - 1] && check(value.charAt(r), value.charAt(c)))) {
+                    isBan[r][c] = true;
+                }
+            }
+        }
+        return isBan[0][length - 1];
+    }
+
+    private boolean check(char left, char right) {
+        return left == '(' && right == ')' || left == '{' && right == '}' || left == '[' && right == ']';
     }
 
     private static class Node {
-        int val;
-        Node next;
+        int data;
+        Node left, right;
 
         public Node() {
         }
 
-        public Node(int val, Node next) {
-            this.val = val;
-            this.next = next;
+        public Node(int newData) {
+            left = right = null;
+            int data;
         }
     }
 }
